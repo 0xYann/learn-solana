3c817c9fdd0ea3e5efaaa977447754ecf175a374
0xYann <yann.gabbud@proton.me>
2023-08-10 10:30:16 +0200

    simple transfer

--- src/simple-transfert.ts
+++ src/simple-transfert.ts
@@ -1,5 +1,4 @@
 import web3 = require('@solana/web3.js')
-import { sign } from 'crypto'
 import Dotenv from 'dotenv'
 Dotenv.config()
 
@@ -8,10 +7,14 @@ const PROGRAM_DATA_ADDRESS = 'Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod'
 
 async function main() {
     const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
-    const payer = initializeKeypair()
+    const sender = initializeKeypair()
+    const receiver = web3.Keypair.generate()
+    const amount = web3.LAMPORTS_PER_SOL*1
 
-    await connection.requestAirdrop(payer.publicKey, web3.LAMPORTS_PER_SOL*1)
-    await pingProgram(connection, payer)
+    // await connection.requestAirdrop(sender.publicKey, amount)
+    await printBalances(connection, sender, receiver)
+    await transferSol(connection, sender, receiver, amount)
+    await printBalances(connection, sender, receiver)
 }
 
 function initializeKeypair(): web3.Keypair {
@@ -21,21 +24,13 @@ function initializeKeypair(): web3.Keypair {
     return keypairFromSecretKey
 }
 
-async function pingProgram(connection: web3.Connection, payer: web3.Keypair) {
+async function transferSol(connection: web3.Connection, sender: web3.Keypair, receiver: web3.Keypair, amount: number) {
     const transaction = new web3.Transaction()
 
-    const programId = new web3.PublicKey(PROGRAM_ADDRESS)
-    const programDataPubkey = new web3.PublicKey(PROGRAM_DATA_ADDRESS)
-
-    const instruction = new web3.TransactionInstruction({
-        keys: [
-            {
-                pubkey: programDataPubkey,
-                isSigner: false,
-                isWritable: true
-            },
-        ],
-        programId
+    const instruction = web3.SystemProgram.transfer({
+        fromPubkey: sender.publicKey,
+        toPubkey: receiver.publicKey,
+        lamports: amount
     })
 
     transaction.add(instruction)
@@ -43,10 +38,18 @@ async function pingProgram(connection: web3.Connection, payer: web3.Keypair) {
     const signature = await web3.sendAndConfirmTransaction(
         connection,
         transaction,
-        [payer]
+        [sender]
     )
 
     console.log(`You can view your transaction on the Solana Explorer at:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+}
+
+async function printBalances(connection: web3.Connection, sender: web3.Keypair, receiver: web3.Keypair) {
+    const senderBalance = await connection.getBalance(sender.publicKey)
+    const receiverBalance = await connection.getBalance(receiver.publicKey)
+
+    console.log('Sender balance:  ', senderBalance)
+    console.log('Receiver balance:', receiverBalance)
 }
 
 main().then(() => {
